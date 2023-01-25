48f4aae06c2006207faa0a7dc879300a5c879bb1
arman <a.hasheminia@tiara.studio>
2023-01-26 02:10:26 +0330

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_todo_app
+
+Flutter todo-app with GetX and clean architecture
---
+++ android/app/src/main/kotlin/com/arman/hashemi/flutter_todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arman.hashemi.flutter_todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
